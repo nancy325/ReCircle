5cab5ba12f1b86150cbbf79c49bdc0a89719816d
pranjalpatil22 <pranjalpatilk22@gmail.com>
2025-09-06 16:10:12 +0530

    xyz

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,31 @@
-<<<<<<< HEAD
 
-=======
->>>>>>> 64960f6b607050b3a794e2b947ac3aa8523990f0
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const authRoutes = require('./routes/authRoutes'); 
 const product = require('./routes/productRoutes');
+const cart = require('./routes/cartRoutes');
 // const db = require('./db');
 
 // const userRoutes = require("./routes/userRoutes");
 
-<<<<<<< HEAD
+
 
 
 const app = express();
 app.use(express.json());
 
 
-=======
-const app = express();
+
+
 app.use(express.json());
 
-// Routes
->>>>>>> 64960f6b607050b3a794e2b947ac3aa8523990f0
+
 app.use("/api/auth", authRoutes);
 app.use("/api/products", product);
+app.use("/api/cart", cart);
 
 const db = require('./db');
-
-<<<<<<< HEAD
 // Example query
 db.query('SELECT 1 + 1 AS solution', (err, results) => {
   if (err) throw err;
@@ -39,8 +35,6 @@ db.query('SELECT 1 + 1 AS solution', (err, results) => {
 const productRoutes = require('./routes/productRoutes');
 app.use('/products', productRoutes);
 
-=======
->>>>>>> 64960f6b607050b3a794e2b947ac3aa8523990f0
 
 
 
--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const { authenticateToken } = require('../middleware/auth');
-const{ getOrCreateCart } = require('../controller/cartController');
+const{ getCart } = require('../controller/cartController');
 
 const router = express.Router();
 
-router.get('/', authenticateToken, getOrCreateCart);
+router.get('/', authenticateToken, getCart);
 
 module.exports = router;
