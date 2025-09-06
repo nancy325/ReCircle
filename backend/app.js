d632e045b9c16bad32d6a20da71b06d589da47bc
pranjalpatil22 <pranjalpatilk22@gmail.com>
2025-09-06 10:56:45 +0530

    2 files

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/users", userRoutes);
+
+module.exports = app;
